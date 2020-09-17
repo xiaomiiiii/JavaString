1d41c8a843b0003d5df05c9ee1586a63670bc343
xiaomiiiii <liuxiaomisept@gmail.com>
2020-09-16 21:32:51 -0700

    Check palindrome

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,9 @@ public class Main {
         // Q1. Count vowels
         String s1 = "It is a String. I need to count all the vowels in this sentence.";
         Vowels.countVowels(s1);
+
+        // Q2. Check Palindrome
+        String s2 = "Was it a car or a cat I saw?";
+        Palindrome.isPalindrome(s2);
     }
 }
---
+++ src/Palindrome.java
@@ -0,0 +1,19 @@
+public class Palindrome {
+    String s = "Was is a car or a cat I saw?";
+
+    public static void isPalindrome(String s) {
+        s = s.toLowerCase().replaceAll("[^0-9|a-z]", "");
+
+        StringBuilder sb = new StringBuilder("");
+        for (int i = s.length() - 1; i >= 0; i--) {
+            sb.append(s.charAt(i));
+        }
+        String newS = sb.toString();
+
+        if (s.equals(newS)) {
+            System.out.println("This String is a palindrome.");
+        } else {
+            System.out.println("This String is NOT a palindrome.");
+        }
+    }
+}
