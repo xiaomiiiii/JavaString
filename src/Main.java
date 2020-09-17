93b8c2143918cdaf8c41ec1aaf520f0631921bf2
xiaomiiiii <liuxiaomisept@gmail.com>
2020-09-16 20:52:00 -0700

    Count vowels

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // Q1. Count vowels
+        String s1 = "It is a String. I need to count all the vowels in this sentence.";
+        Vowels.countVowels(s1);
+    }
+}
---
+++ src/Vowels.java
@@ -0,0 +1,26 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Vowels {
+    public String s = "It is a String. I need to count all the vowels in this sentence.";
+
+    public static void countVowels(String s) {
+        Set<Character> vowels = new HashSet<>();
+        vowels.add('a');
+        vowels.add('e');
+        vowels.add('i');
+        vowels.add('o');
+        vowels.add('u');
+
+        s = s.toLowerCase();
+        int cnt = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (vowels.contains(s.charAt(i))) {
+                cnt++;
+            }
+        }
+
+        System.out.println("There are " + cnt + " vowels in this sentence.");
+    }
+}
